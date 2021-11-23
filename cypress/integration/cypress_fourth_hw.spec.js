b4ea0d07d64843257af6c6a85cd3d6996ca61d27
NikoLazur <lazur.niko@gmail.com>
2021-11-23 15:25:53 +0200

    added some tests

--- cypress/integration/cypress_fourth_hw.spec.js
+++ cypress/integration/cypress_fourth_hw.spec.js
@@ -1,11 +1,31 @@
 describe ('', () => {
 
+  const book = {
+    name: 'Speaking JavaScript',
+    author: 'Axel Rauschmayer',
+    publisher: "O'Reilly Media"
+  }
+
   beforeEach(() => {
     cy.login();
   })
 
-  it('', () => {
-   
-  })
+  it('Username should display at page when login', () => {
+    cy.get('#userName-value')
+      .should('contain', 'Niko');
+  });
 
+  it('User be able to find the book by name in searchbar', () => {
+    cy.contains('#item-2', 'Book Store')
+      .click();
+    cy.url().should('include', '/books');
+    cy.get('#searchBox')
+      .type('Speaking JavaScript{enter}');
+    cy.get('.ReactTable')
+      .should('contain', book.name, book.author, book.publisher);
+  });
+
+  it('User be able to add the book to collection', () => {
+
+  })
 })
