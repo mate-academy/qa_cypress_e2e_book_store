f19553d4cb63236d9fd0e7b30779b417a57522ca
NikoLazur <lazur.niko@gmail.com>
2021-11-23 17:24:42 +0200

    added solution for basic task

--- cypress/integration/cypress_fourth_hw.spec.js
+++ cypress/integration/cypress_fourth_hw.spec.js
@@ -22,10 +22,45 @@ describe ('', () => {
     cy.get('#searchBox')
       .type('Speaking JavaScript{enter}');
     cy.get('.ReactTable')
-      .should('contain', book.name, book.author, book.publisher);
-  });
+      .should('contain', book.name, book.author, book.publisher)
+    cy.contains('.rt-tr.-odd', book.name)
+      .should('contain', book.author)
+      .and('contain', book.publisher);
+  })
 
-  it('User be able to add the book to collection', () => {
+  it('User be able to add book to collection', () => {
+    cy.contains('#item-2', 'Book Store')
+      .click();
+    cy.contains('.action-buttons', book.name)
+      .click();
+    cy.get('#description-wrapper')
+      .should('contain', 'Like it or not, JavaScript is everywhere');
+    cy.get('#author-wrapper')
+      .should('contain', book.author);
+    cy.get('#publisher-wrapper')
+      .should('contain', book.publisher);
+    cy.get('#pages-wrapper')
+      .should('contain', '460');
+    cy.contains('#addNewRecordButton', 'Add To Your Collection')
+        .click();
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Book already present in the your collection!`)
+    })
+  })
 
+  it('User be able to delete book from collection', () => {
+    cy.contains('#item-3', 'Profile')
+      .click();
+    cy.url()
+      .should('include', '/profile');
+    cy.contains('.rt-tr.-odd', book.name)
+      .should('contain', book.author)
+      .and('contain', book.publisher)
+      .find('#delete-record-undefined')
+      .click();
+    cy.get('#closeSmallModal-ok')
+      .click();
+    cy.contains(book.author, book.name, book.publisher)
+      .should('not.exist');
   })
 })
