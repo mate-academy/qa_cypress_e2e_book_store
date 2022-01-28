b97adbcad9c0d0122d9506d2c32b87d4f8655f9c
Katy Mozharovska <katy.mozharovska@gmail.com>
2022-01-28 15:06:37 +0200

    fixed

--- cypress/integration/signIn.spec.js
+++ cypress/integration/signIn.spec.js
@@ -12,6 +12,12 @@ describe('User can', () => {
     publisher: "O'Reilly Media"
   };
 
+  const book2 = {
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    publisher: "O'Reilly Media"
+  };
+
 
   it('successfully login', () => {
     cy.get('#userName-value').should('contain', user.username)
@@ -28,18 +34,26 @@ describe('User can', () => {
     cy.on('window:alert', (str) => {
       expect(str).to.equal(`Book added to your collection.`)
     });
+    cy.visit('profile');
+    cy.get('#delete-record-undefined').click();
+    cy.get('#closeSmallModal-ok').click('center');
   });
 
   it(`delete book from user's shopping list`, () => {
+    cy.visit('books');
+    cy.get('#searchBox').type(book2.title);
+    cy.contains('.rt-tr.-odd', book2.title)
+    .should('contain', book2.author)
+    .and('contain', book2.publisher);
+    cy.get(`[id$="You Don't Know JS"]`).click();
+    cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
     cy.visit('profile');
-    cy.contains('.rt-tr.-odd', book.title)
-    .should('contain', book.author)
-    .and('contain', book.publisher);
+    cy.contains('.rt-tr.-odd', book2.title)
+    .should('contain', book2.author)
+    .and('contain', book2.publisher);
     cy.get('#delete-record-undefined').click();
-    cy.get('#closeSmallModal-ok')
-    .click();
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(`Book deleted.`)
-    });
+    cy.get('#closeSmallModal-ok').click('center');
+    cy.contains(book2.title)
+      .should('not.exist');
   });
 });
