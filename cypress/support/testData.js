07c27994f9c162e5a7af5748b012be521d467e7e
nicatino <epicjew.nougoi@gmail.com>
2022-10-06 21:20:11 +0300

    Solution

---
+++ cypress/support/testData.js
@@ -0,0 +1,14 @@
+const book = {
+  title: 'Speaking JavaScript',
+  author: 'Axel Rauschmayer',
+  publisher: 'O\'Reilly Media',
+  isbn: '9781449365035'
+}
+
+const user = {
+  username: 'Sup3rT3st3r!',
+  password: 'Sup3rT3st3r!',
+  userId: '376a8456-ee2c-4e8b-9a22-6b63b5491db5'
+};
+
+export { book, user };
