173d761ed0d3989dd95623bfe9b040f93e3a4d49
d.borovskyi <dmytro.borovskyi.qa@gmail.com>
2022-10-06 15:39:44 +0300

    Solution (Basic and Advanced)

---
+++ cypress/support/testData.js
@@ -0,0 +1,20 @@
+const user = {
+  firstName: 'Solomon',
+  lastName: 'Bush',
+  username: 'rurapo',
+  password: 'Pa$$w0rd!'
+};
+
+const book = {
+  title: 'Speaking JavaScript',
+  author: 'Axel Rauschmayer',
+  publisher: 'O\'Reilly Media',
+  ISBN: '9781449365035'
+}
+
+const alertMessage = {
+  success: 'Book added to your collection.',
+  delete: 'Book deleted.'
+}
+
+export {user, book, alertMessage};
