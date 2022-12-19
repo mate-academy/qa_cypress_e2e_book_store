99eddb50440003df8910dda4c29a73aec48a6312
Roman Paliy <paliygamingua@gmail.com>
2022-12-19 21:59:39 +0200

    mySolution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (userName = 'Tester1337', password = 'Tester!337') => {
+    cy.request({
+        method: 'POST',
+        url: 'https://demoqa.com/Account/v1/Login',
+        body: {
+            userName: userName,
+            password: password,
+        },  
+    }).then((response) => {
+        cy.setCookie('token', response.body.token);
+        cy.setCookie('userID', response.body.userId);
+        cy.setCookie('expires', response.body.expires);
+        cy.setCookie('userName', response.body.username);
+    });
+});
