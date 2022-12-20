345d89a97de837ce7b290da3dfea23a0129490ec
Karolina Vlasova <carolina150897@gmail.com>
2022-12-20 16:40:16 +0100

    new solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -34,8 +34,8 @@ describe('Book Store app', () => {
       .click();
     cy.contains('a', 'Speaking JavaScript')
       .click();
-    cy.get('.form-label')
-      .should('contain', 'JavaScript');
+    cy.get('#description-wrapper')
+      .should('contain', 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o');
     cy.get('.text-right > #addNewRecordButton')
       .click({force:true});
     cy.on('window:alert', (str) => {
