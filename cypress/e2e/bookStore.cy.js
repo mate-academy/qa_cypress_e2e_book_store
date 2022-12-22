322bfc6930600c096e9cd38e83a91ef6b679d6ef
Sergii Sinichenko <sinichenkosergey@gmail.com>
2022-12-22 10:09:35 +0200

    solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -2,10 +2,35 @@
 
 describe('Book Store app', () => {
   before(() => {
-    
+
+    cy.viewport(1200, 1200)
+    cy.visit('https://demoqa.com/login')
   });
 
-  it('', () => {
-    
+  it('login, add to cart, delete from cart', () => {
+    cy.get('#userName').type('lexif');
+    cy.get('#password').type('Lexif@2022');
+    cy.get('#login').click();
+    cy.get('[id="userName-value"]').should('contain', 'lexif');
+    cy.url().
+    should('include', 'profile');
+    cy.get(':nth-child(6) > .element-list > .menu-list > #item-2').click();
+    cy.get('[id="searchBox"]').type('Speaking JavaScript');
+    cy.get('[href="/books?book=9781449365035"]').click();
+    cy.get('#description-wrapper > .col-md-9 > #userName-value').
+    should('contain', 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o');
+    cy.get('.text-right > #addNewRecordButton').click();
+    cy.wait(4000);
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`)
+  });
+    cy.visit('https://demoqa.com/profile');
+    cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)').should('contain', 'Speaking JavaScript');
+    cy.get('[id="delete-record-undefined"]').click();
+    cy.wait(4000);
+    cy.get('#closeSmallModal-ok').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book deleted.`)
+  });
   });
 });
