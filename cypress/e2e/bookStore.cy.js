abf113360b673ecd4077739be110b3698ea89c4b
Anna Simko <108935534+Ann1120-ua@users.noreply.github.com>
2023-01-27 13:50:03 -0600

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,52 @@
 /// <reference types='cypress' />
-
+import Login from "../PageObjects/Login";
+import BookStore from "../PageObjects/BookStore";
+import Profile from "../PageObjects/ProfilePage";
 describe('Book Store app', () => {
-  before(() => {
-    
-  });
 
-  it('', () => {
+  it('should allow an existing user to login, add a book and delete a book', () => {
     
+    cy.viewport(1024, 768);
+    cy.visit('/login');
+  
+    const username = "test_anna_s";
+    const password = "Test123@";
+     
+
+    const loginPage = new Login();
+    loginPage.setUserName(username);
+    loginPage.setPassword(password);
+    loginPage.clickSubmit() 
+    cy.wait(4000);
+    cy.url().should('include', '/profile');
+  
+    
+
+    const bookStore = new BookStore();
+    bookStore.navigate();
+    bookStore.typeSearchBox();
+    bookStore.clickOnBook();
+    bookStore.verifyBook();
+    bookStore.clickOnWidget();
+    cy.wait(4000);
+    bookStore.addToCollection();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection`)
+  })
+  
+  cy.go('back')
+  cy.wait(4000)
+
+   const profile = new Profile();
+   profile.navigate();
+   profile.verifyJavaBook();
+   profile.deleteBook();
+ 
+  cy.wait(4000);
+  cy.get('#closeSmallModal-ok').click();
+  cy.on('window:alert', (str) => {
+    expect(str).to.equal(`Book deleted.`)
+    });
   });
 });
