03576f28e92442a8242d79eeb5a33cc4bf3c32e3
Yuliia Sokolova <yulyasokolova.lvov@gmail.com>
2023-02-09 00:46:21 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require('cypress');
+const faker = require("faker");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportHeight: 1080,
+    viewportWidth: 1340,
     setupNodeEvents(on, config) {
     },
   },
