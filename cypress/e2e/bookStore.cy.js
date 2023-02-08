4d945cde281fbc7a45cc4a6e2451d4840fe2ba69
Serhii Fartushnyi <far2shok@gmail.com>
2023-02-08 19:14:04 +0200

    Solutions

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,108 @@
 /// <reference types='cypress' />
+/// <reference types="../support"/>
+
+
+const user = {
+  username: "Adam",
+  password: "12345Qwert!"
+};
+
+const book ={
+  name: 'Speaking JavaScript',
+  author: 'Axel Rauschmayer',
+  publisher: 'O\'Reilly Media',
+  description: 'Like it or not, JavaScript is everywhere',
+};
 
 describe('Book Store app', () => {
   before(() => {
-    
+    cy.visit('/login')
   });
 
-  it('', () => {
+  it('Should provide abbility to log in', () => {
+    cy.get('[placeholder="UserName"]')
+      .type(user.username);
+    cy.get('[placeholder="Password"]')
+      .type(user.password);
+
+    cy.get("#login")
+      .click();
+
+    cy.url().should('include', '/profile');
+  
+    cy.get('#userName-value').should('have.text', 'Adam');
+});
+
+  it('Should provide an abbility to Search for the book and add to chart', () => {
     
+    cy.visit('/login');
+
+    cy.login(user.username, user.password);
+
+    cy.wait(1000);
+
+    cy.contains('[id=item-2]', 'Book Store').click();
+
+    cy.contains('#userName-value', user.username);
+
+    cy.findByPlaceholder('Type to search').type(book.name);
+
+    cy.contains ('[role="row"]', book.name)
+      .should('contain', book.author)
+      .and('contain', book.publisher);
+
+    cy.contains('a', book.name)
+      .click();
+
+    cy.contains('#description-wrapper', 'Description')
+      .should('contain', book.description);
+
+    cy.contains('#addNewRecordButton', 'Add To Your Collection')
+      .click();
+
+    cy.on('window:alert', (alert) => {
+      expect(alert)
+      .to.equal('Book added to your collection.');
+
+    });
+
+      cy.get('#submit')
+        .click();
+
+      cy.contains('.playgound-header', 'Login')
+});
+
+it('Should provide an abbility to delete the book ', () => {
+    
+  cy.visit('/login');
+
+  cy.get('[placeholder="UserName"]')
+      .type(user.username);
+
+    cy.get('[placeholder="Password"]')
+      .type(user.password);
+
+    cy.get("#login")
+      .click();
+
+    cy.url().should('include', '/login');
+  
+    cy.get('#userName-value').should('have.text', 'Adam');
+
+    cy.get('[id="delete-record-undefined"]')
+      .click();
+
+    cy.get('.modal-body')
+      .should('contain.text', 'Do you want to delete this book?');
+
+    cy.get('#closeSmallModal-ok')
+      .click();
+
+    cy.on('window:alert', (alert) => {
+      expect(alert)
+      .to.equal('Book deleted.');
+    });
+      
   });
+
 });
