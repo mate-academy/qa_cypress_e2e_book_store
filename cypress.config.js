35227c9fdb7428c60b4e16698f7a450cb199f12b
Nataliia Khoruzhenko <natelis.nv@gmail.comm>
2023-02-13 18:23:03 +0200

    book store test added

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
+    baseUrl: 'https://demoqa.com',
+    viewportHeight: 1200,
+    viewportWidth: 1320,
   },
 });
