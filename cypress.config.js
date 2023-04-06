41e56d5b1b726c479dd6fb9c8138547811d3c1b2
Max Nikiforov <maxxnikiforov@gmail.com>
2023-04-06 22:12:31 +0700

    all tests added 1 step

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportWidth: 1000,
+    viewportHeight: 1000,
     setupNodeEvents(on, config) {
     },
   },
--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,59 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
-    
+const user = {
+  name: 'max',
+  password: 'Cy1@Password',
+}
+
+const book = {
+  title: 'Speaking JavaScript',
+  description: 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o',
+}
+
+  beforeEach(() => {
+    cy.visit('/login')
   });
 
-  it('', () => {
+  it('should be possible to log in for existed user', () => {
+    cy.get('[placeholder="UserName"]')
+      .type(`${user.name}`)
+
+    cy.get('[placeholder="Password"]')
+      .type(`${user.password}`)
+
+    cy.contains('button', 'Login').click()
+
+    cy.get('#userName-value')
+      .should('contain.text', `${user.name}`)
+
+    cy.get('#gotoStore')
+      .click()
+
+    cy.contains('a', 'Speaking JavaScript')
+      .click()
+
+    cy.get('#title-wrapper')
+      .should('contain.text', book.title)
+
+    cy.get('#description-wrapper')
+      .should('contain.text', book.description)
+
+    cy.get('#addNewRecordButton')
+      .click()
+
+    cy.visit('/profile')
+
+    cy.contains('a', 'Speaking JavaScript')
+      .should('exist')
     
+    cy.contains('button', 'Delete All Books')
+     .click()
+
+    cy.get('#closeSmallModal-ok')
+      .click()
+
+    cy.contains('a', 'Speaking JavaScript')
+      .should('not.exist')
   });
 });
