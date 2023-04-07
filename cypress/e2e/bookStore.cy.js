9b7b3a0a7ff37c63f476e4f0acc01e4c73f53719
Oleksandr Stryzhavchuk <sstrizhavchuk@gmail.com>
2023-04-07 18:36:56 +0200

    solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,39 @@
 /// <reference types='cypress' />
 
+const { generateData } = require('../support/generate');
+
 describe('Book Store app', () => {
-  before(() => {
-    
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.login();
   });
 
-  it('', () => {
-    
+  it('should check if the user is logged in', () => {
+    const { userName } = generateData();
+
+    cy.get('#userName-value').should('contain.text', userName);
+  });
+
+  it('should allow to add book', () => {
+    const { bookName, description } = generateData();
+
+    cy.url().should('include', '/profile');
+    cy.get('#item-2 span').contains('Book Store').click();
+    cy.get('#searchBox').type(bookName);
+    cy.contains('a', bookName).click();
+    cy.get('#description-wrapper').should('contain.text', description);
+    cy.get('.text-right > #addNewRecordButton').click();
+    cy.once('window:alert', (str) => {
+      expect(str).to.eq('Book added to your collection.');
+    });
+  });
+
+  it('should allow to delete book', () => {
+    const { bookName } = generateData();
+
+    cy.url().should('include', '/profile');
+    cy.get('#item-3 span').contains('Profile').click();
+    cy.contains('a', bookName).should('exist');
+    cy.get('#delete-record-undefined').click().get('#closeSmallModal-ok').click();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types='cypress' />
+
+const { generateData } = require('../support/generate');
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
+});
+
+// Cypress.Commands.add('login', () => {
+//   const { userName, password } = generateData();
+
+//   cy.request({
+//     method: 'POST',
+//     url: '/Account/v1/Login',
+//     body: {
+//       userName: userName,
+//       password: password
+//     }
+//   })
+//     .then(response => {
+//       cy.setCookie('token', response.body.token)
+//       cy.setCookie('userId', response.body.userId)
+//       cy.setCookie('username', response.body.username)
+//       cy.setCookie('expires', response.body.expires)
+//     })
+// })
+
+Cypress.Commands.add('login', () => {
+  const { userName, password } = generateData();
+
+  cy.findByPlaceholder('UserName').type(userName);
+  cy.findByPlaceholder('Password').type(password);
+
+  cy.get('#login').click();
+})
