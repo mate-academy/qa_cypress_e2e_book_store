f0bc197ec8b1bfcd00830cb7a9bbd79f47dd8313
Maryna Oliinyk <ukrmartin22@gmail.com>
2023-04-13 21:33:33 +0300

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,30 @@
 /// <reference types='cypress' />
 
+import { user, book } from '../support/testData';
+
 describe('Book Store app', () => {
   before(() => {
-    
+    cy.visit('/login');
+    cy.viewport(1280, 800);
   });
 
-  it('', () => {
-    
+  it('should provide an ability to login', () => {
+    cy.get('#userName').type(user.username);
+    cy.get('#password').type(user.password);
+    cy.contains('.text-right button', 'Login').click();
+    cy.url().should('equal', `${Cypress.config().baseUrl}/profile`);
+    cy.get('#userName-value').should('contains.text', user.username);
+    cy.get('#gotoStore').click();
+    cy.get('#searchBox').type(`${book.title}{enter}`);
+    cy.contains('a', book.title).click();
+    cy.get('#author-wrapper > .col-md-9 > #userName-value').should('contain', book.author);
+    cy.get('#publisher-wrapper > .col-md-9 > #userName-value').should('contain', book.publisher);
+    cy.contains('.btn', 'Add To Your Collection').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`)
+    });
+    cy.contains('.btn', 'Profile').click();
+    cy.get('#delete-record-undefined > svg > path').click();
+    cy.get('#closeSmallModal-ok').click();
   });
 });
---
+++ cypress/support/testData.js
@@ -0,0 +1,10 @@
+export const user = {
+    username: 'MarynaOl',
+    password: '12345Mar@)'
+}
+
+export const book = {
+    title: 'Speaking JavaScript',
+    author: 'Axel Rauschmayer',
+    publisher: 'O\'Reilly Media'
+}
