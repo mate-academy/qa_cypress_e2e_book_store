e980e7d975ede1360412c7bf527a8456dcc4bb93
HalynaKomar <komarg007@gmail.com>
2023-04-13 18:48:20 +0300

    [Halyna Komar] bookStore

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/login',
+    viewportWidth: 1100,
+    viewportHeight: 1300,
     setupNodeEvents(on, config) {
     },
   },
