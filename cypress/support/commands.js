784b704f74d94ad6a812cd2e42d261e957056df4
Yurii <dmukhailo@gmail.com>
2023-04-13 17:52:57 +0300

    Solution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (username, password) => {
+  cy.request({
+    method: "POST",
+    url: "https://demoqa.com/Account/v1/Login", // baseUrl is prepend to URL
+    body: {
+      userName: "dmukhailo123",
+      password: "Dmukhailo123!",
+    },
+  }).then((response) => {
+    cy.setCookie("token", response.body.token);
+    cy.setCookie("userName", response.body.username);
+    cy.setCookie("userID", response.body.userId);
+    cy.setCookie("expires", response.body.expires);
+  });
+});
