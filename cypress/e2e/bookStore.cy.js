cb4bdee26ed8312936bf03e9fdb42c8d4baae226
Veronika Kondratova <nkond222@gmail.com>
2023-04-13 17:28:35 +0300

    E2E test for Book Store

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,51 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
-    
+  it('Login with valid creds', () => {
+    cy.visit('/login');
+
+    cy.get('#userName').type('nkond.test');
+    cy.get('#password').type('Passwprd!123');
+    cy.contains('.text-right button', 'Login').click();
+
+    cy.get('#userName-value').should('contain.text', 'nkond.test');
+    cy.assertPageUrl('/profile');
   });
 
-  it('', () => {
-    
+  it('Add Book', () => {
+    cy.visit('/login');
+
+    cy.get('#userName').type('nkond.test');
+    cy.get('#password').type('Passwprd!123');
+    cy.contains('.text-right button', 'Login').click();
+
+    cy.wait(4000);
+
+    cy.visit('/profile');
+    cy.get('#gotoStore').click();
+
+    cy.findByPlaceholder('Type to search').type('Speaking JavaScript{enter}');
+    cy.contains('a', 'Speaking JavaScript').click();
+    cy.contains('.btn', 'Add To Your Collection').click();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`)
+    });
+  });
+
+  it('Delete Book', () => {
+    cy.visit('/login');
+
+    cy.get('#userName').type('nkond.test');
+    cy.get('#password').type('Passwprd!123');
+    cy.contains('.text-right button', 'Login').click();
+
+    cy.wait(4000);
+
+    cy.visit('/profile');
+
+    cy.contains('a', 'Speaking JavaScript').should('exist');
+    cy.contains('.rt-tbody', 'Speaking JavaScript').find('#delete-record-undefined').click();
+    cy.get('#closeSmallModal-ok').click();
   });
 });
