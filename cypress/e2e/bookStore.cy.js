4496b92b5b33123774c43a1c09e2decc9b4e339b
Serhii-Cherh <chiper9@gmail.com>
2023-04-20 22:01:42 +0300

    solution is updated

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -3,28 +3,28 @@
 
 describe('Book Store app', () => {
 
-  it('login', () => {
+  it('should let user to login', () => {
     cy.login();
     cy.visit('/profile');
     cy.get('#userName-value').should('contain', 'serhio1234');
     cy.url().should('include', '/profile');
   });
 
-  it('add book', () => {
+  it('should add a book to the collection', () => {
     cy.viewport(750, 600);
     cy.login();
     cy.visit('/profile');
     cy.contains('.btn', 'Book Store').click();
     cy.get('[placeholder="Type to search"]').click().type('Speaking JavaScript');
     cy.contains('O\'Reilly Media');
-    cy.contains('.action-buttons', 'Speaking JavaScrip').click();
+    cy.contains('.action-buttons', 'Speaking JavaScript').click();
     cy.contains('.btn', 'Add To Your Collection').click();
     cy.on('window:alert', (str) => {
       expect(str).to.equal(`Book added to your collection.`)
   })
   });
 
-    it('delete book', () => {
+    it('should delete a book from the collection', () => {
     cy.login();
     cy.visit('/profile');
     cy.contains('Speaking JavaScript');
