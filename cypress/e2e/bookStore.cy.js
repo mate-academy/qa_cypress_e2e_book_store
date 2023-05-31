bc1157ae26affc1044cd24b1422e6d835478f2a3
Oleksii Shypovalov <asipovalov9@gmail.com>
2023-05-31 14:16:31 +0300

    Solution1

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,59 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
-    
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/login')
   });
 
   it('', () => {
+    const userName = 'TesterUser';
+    const password = 'dsaEWQ321!';
+    const bookName = 'Speaking JavaScript'
+
+    cy.get('#userName')
+      .type(userName);
+
+    cy.get('#password')
+      .type(password);
+
+    cy.contains('button', 'Login')
+      .click();
+
+    cy.get('#userName-value')
+      .should('contain', userName);
     
+    cy.url()
+      .should('eq', 'https://demoqa.com/profile');
+
+    cy.contains('#item-2', 'Book Store')
+      .click();
+
+    cy.get('#searchBox')
+      .type(bookName);
+
+    cy.contains('a', bookName)
+      .click();
+
+    cy.contains('.form-label', 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o')
+      .should('exist');
+
+    cy.contains('button', 'Add To Your Collection')
+      .click();
+
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal(`Book added to your collection.`)
+    });
+
+    cy.contains('#item-3', 'Profile')
+      .click();
+
+    cy.contains('.ReactTable', bookName)
+      .should('exist');
+
+    cy.get('#delete-record-undefined')
+      .click();
+
+    cy.get('#closeSmallModal-ok')
+      .click();
   });
 });
