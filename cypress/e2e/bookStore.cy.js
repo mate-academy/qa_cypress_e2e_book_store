2310805be8d05fa2363cd0affcab39d61f0e6d0a
Dmytro <dmytrywork81@gmail.com>
2023-06-01 14:29:49 +0300

    01/06/2023

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -2,10 +2,22 @@
 
 describe('Book Store app', () => {
   before(() => {
-    
+    cy.visit('/login');
+    cy.get('#userName').type('haha.test11');
+    cy.get('#password').type('Passwprd!123');
+    cy.contains('.text-right button', 'Login').click();
+    cy.get('#userName-value').should('contain.text', '');
+    cy.assertPageUrl('/profile');
   });
 
-  it('', () => {
-    
+  it('Add Book', () => {
+    cy.visit('/profile');
+    cy.get('#gotoStore').click({ force: true });
+    cy.get('[placeholder="Type to search"]').type('Speaking JavaScript{enter}');
+    cy.contains('a', 'Speaking JavaScript').click();
+    cy.contains('.btn', 'Add To Your Collection').click({ force: true });
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Book already present in the your collection!');
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
+});
+
+Cypress.Commands.add('assertPageUrl', (url) => {
+  cy.url()
+      .should('include', Cypress.config().baseUrl + url)
+});
