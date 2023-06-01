fbc9a5c3245e5b0b929c240277240af3b3a7c898
katzamkova <katerynadashko95@gmail.com>
2023-06-01 14:48:07 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
+/// <reference types="cypress" />
+
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportWidth: 1960,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
     },
   },
