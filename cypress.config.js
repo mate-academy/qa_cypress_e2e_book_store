7d8d5ca02b86053de0a2297db7ab736731e59d68
Artem <protiv55@gmail.com>
2023-06-01 18:04:45 +0300

    Solition

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
+    baseUrl: 'https://demoqa.com/login',
+    viewportHeight: 1080,
+    viewportWidth: 1980,
+
   },
 });
