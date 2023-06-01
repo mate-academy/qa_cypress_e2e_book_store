6653dcd0779be3113286a99c78ca767bb1097b18
Olesia Mykytiuk <olesichkapysuk@gmail.com>
2023-06-01 21:46:00 +0300

    Resolved tasks in bookStore.cy.js

--- cypress/support/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types='cypress' />
-declare namespace Cypress{
-    interface Chainable<Subject> {
-        findByPlaceholder(placeholder: string): Chainable<any>
-        login(username: string, password: string): Chainable<any>
-    }
-}
