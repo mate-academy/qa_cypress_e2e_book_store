94d75e3034a48954a0f2e9cd88fc4fe9f4eda707
Olesia Mykytiuk <olesichkapysuk@gmail.com>
2023-06-01 21:41:39 +0300

    Resolved tasks in bookStore.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeigh: 1080,
+    viewportWidth: 1280,
     setupNodeEvents(on, config) {
     },
   },
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types='cypress' />
+declare namespace Cypress{
+    interface Chainable<Subject> {
+        findByPlaceholder(placeholder: string): Chainable<any>
+        login(username: string, password: string): Chainable<any>
+    }
+}
