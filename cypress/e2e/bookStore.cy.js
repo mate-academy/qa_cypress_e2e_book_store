7305e5ee1dddb5ab40348b98dd548ad6a349defd
Roman Vytrykush <qwertywo2000@gmail.com>
2023-06-04 16:16:47 +0300

    bookStore tests

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,80 @@
 /// <reference types='cypress' />
 
-describe('Book Store app', () => {
-  before(() => {
-    
-  });
+const username = 'Ace';
+const password = '!23Qweasd';
 
-  it('', () => {
-    
-  });
+it('Log in', () => {
+  cy.login(username, password);
+
+  cy.get('#userName-value')
+  .should('contain', username);
+});
+
+it('Log in with API', () => {
+  cy.loginAPI(username, password);
+
+  cy.getCookie('token').should('exist');
+});
+
+it('Add Book', () => {
+  cy.loginAPI(username, password);
+
+  cy.visit('');
+
+  cy.get('#fixedban')
+    .invoke('remove');
+
+  cy.contains('Book Store App')
+    .click();
+
+  cy.get(':nth-child(6) > .element-list > .menu-list > #item-2')
+    .click();
+
+  cy.get('#searchBox')
+    .clear()
+    .type('Speaking Javascript');
+
+  cy.get('.action-buttons')
+    .click();
+
+  cy.get('#fixedban')
+    .invoke('remove');
+
+  cy.get('#description-wrapper > .col-md-9')
+    .should('contain', 'JavaScript');
+
+  cy.contains('Add To Your Collection')
+    .click({ force: true });
+
+  cy.contains('Profile')
+    .click();
+
+  cy.get('.rt-table')
+    .contains('speaking javascript', { matchCase: false });
+
+  cy.on('window:alert', (str) => {
+    expect(str).to.equal('Book added to your collection.')
+  })
+});
+
+it('Delete Book', () => {
+  cy.loginAPI(username, password);
+
+  cy.visit('/profile');
+
+  cy.contains('Profile')
+    .click();
+
+  cy.get('[title = "Delete"]')
+    .click();
+
+  cy.get('#closeSmallModal-ok')
+    .click();
+
+  cy.on('window:alert', (str) => {
+    expect(str).to.equal('Book deleted.')
+  })
+
+  cy.get('.rt-noData')
+  .should('contain', 'No rows found');
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginAPI', (username, password) => {
+    cy.request('POST', 'https://demoqa.com/Account/v1/Login', { userName: username, password: password })
+        .then((response) => {
+            cy.setCookie('token', response.body.token);
+            cy.setCookie('userID', response.body.userId);
+            cy.setCookie('expires', response.body.expires);
+            cy.setCookie('userName', response.body.username);
+
+            expect(response.status).to.eq(200);
+        });
+});
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('');
+
+    cy.contains('Book Store')
+        .click();
+
+    cy.contains('Book Store App')
+        .click();
+
+    cy.contains('Login')
+        .click({ force: true });
+
+    cy.get('#userName')
+        .clear()
+        .type(username);
+
+    cy.get('#password')
+        .clear()
+        .type(password);
+
+    cy.get('#login')
+        .click();
+});
