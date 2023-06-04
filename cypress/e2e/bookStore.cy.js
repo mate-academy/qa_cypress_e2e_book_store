a4c08b2b49fc03227c8c29d6e314edffd913cc5f
Anton Kalnohuz <akalnoguz@gmail.com>
2023-06-04 22:13:54 +0300

    Fixes after review

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -11,70 +11,71 @@ describe('Book Store app', () => {
       name: 'Speaking JavaScript',
       author: 'Axel Rauschmayer',
       publisher: 'O\'Reilly Media',
-      description: 'Like it or not, JavaScript is everywhere'
+      description: 'Like it or not, JavaScript is everywhere',
+      text: 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o'
     }
   };
 
   beforeEach(() => {
-  cy.visit('https://demoqa.com/login');  
+    cy.visit('https://demoqa.com/login');
   });
 
   it('should provide an ability to login', () => {
-  cy.get('#userName')
-  .type(testData.user.username);
+    cy.get('#userName')
+      .type(testData.user.username);
 
-  cy.get('#password')
-  .type(testData.user.password);  
+    cy.get('#password')
+      .type(testData.user.password);
 
-  cy.get('#login')
-  .click();
+    cy.get('#login')
+      .click();
 
-  cy.get('#userName-value')
-  .should('contain', testData.user.username);
-  
-  cy.url()
-  .should('include', '/profile');
+    cy.get('#userName-value')
+      .should('contain', testData.user.username);
+
+    cy.url()
+      .should('include', '/profile');
   });
 
-it('should provide an ability to search for the book and add to cart', () => {
-cy.login(testData.user.username, testData.user.password);
+  it('should provide an ability to search for the book and add to cart', () => {
+    cy.login(testData.user.username, testData.user.password);
 
-//cy.visit('https://demoqa.com/profile');
+    //cy.visit('https://demoqa.com/profile');
 
-cy.contains('#item-2', 'Book Store')
-.click();
+    cy.contains('#item-2', 'Book Store')
+      .click();
 
-cy.get('#searchBox')
-.type(testData.book.name);
+    cy.get('#searchBox')
+      .type(testData.book.name);
 
-cy.contains('a', testData.book.name)
-.click();
+    cy.contains('a', testData.book.name)
+      .click();
 
-cy.contains('.form-label', 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o')
-.should('exist');
+    cy.contains('.form-label', testData.book.text )
+      .should('exist');
 
-cy.contains('button', 'Add To Your Collection')
-.click();
+    cy.contains('button', 'Add To Your Collection')
+      .click();
 
-cy.on('window:alert', (str) => {
-  expect(str).to.equal(`Book added to your collection.`)
-});
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`)
+    });
 
-});
+  });
 
-it('should provide an ability to delete the book from my list', () => {
+  it('should provide an ability to delete the book from my list', () => {
 
-  cy.login();
+    cy.login();
 
-  cy.visit('https://demoqa.com/profile');
+    cy.visit('https://demoqa.com/profile');
 
-  cy.contains('.ReactTable', testData.book.name)
-    .should('exist');
+    cy.contains('.ReactTable', testData.book.name)
+      .should('exist');
 
-  cy.get('#delete-record-undefined')
-    .click();
+    cy.get('#delete-record-undefined')
+      .click();
 
-  cy.get('#closeSmallModal-ok')
-    .click();
-});
+    cy.get('#closeSmallModal-ok')
+      .click();
+  });
 });
