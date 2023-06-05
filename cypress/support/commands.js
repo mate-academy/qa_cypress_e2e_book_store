b95e2f058a300c493c24be76f57a275abc1ca5fa
Anton Kalnohuz <akalnoguz@gmail.com>
2023-06-05 15:05:32 +0300

    Fixes after code review 2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,13 +27,13 @@
 Cypress.Commands.add('login', () => {
   const userName = 'Anton3108';
   const password = 'A12345678a!';
-cy.request('POST', 'https://demoqa.com/Account/v1/Login', {
-  userName: userName,
-  password: password
-}).then(response => {
-  cy.setCookie('token', response.body.token);
-  cy.setCookie('userID', response.body.userId);
-  cy.setCookie('expires', response.body.expires);
 
-});
+  cy.request('POST', 'https://demoqa.com/Account/v1/Login', {
+    userName: userName,
+    password: password
+  }).then(response => {
+    cy.setCookie('token', response.body.token);
+    cy.setCookie('userID', response.body.userId);
+    cy.setCookie('expires', response.body.expires);
+  });
 });
