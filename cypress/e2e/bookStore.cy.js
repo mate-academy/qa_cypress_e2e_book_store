9a9e2e7f4127d11d276c186e264aeb6146804e5e
Julia14012000 <Juliamilan1414@gmail.com>
2023-07-14 08:28:39 +0300

    comment

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,49 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.login();
   });
 
-  it('', () => {
+  const name = 'Speaking JavaScript';
+  const author = 'Axel Rauschmayer';
+  const publisher = 'O\'Reilly Media';
 
+  it('should provide an ability to find and add book', () => {
+    cy.contains('#item-2', 'Book Store').click();
+
+    cy.url().should('include', '/books');
+
+    cy.get('#searchBox').type('Speaking JavaScript{enter}');
+
+    cy.get('.ReactTable').should('contain', name, author, publisher);
+
+    cy.contains('.rt-tr.-odd', name)
+      .should('contain', author)
+      .and('contain', publisher);
+
+    cy.contains('.action-buttons', name).click();
+
+    cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Book added to your collection.');
+    });
+  });
+
+  it('should provide an ability to delete book', () => {
+    cy.contains('#item-3', 'Profile').click();
+
+    cy.url().should('include', '/profile');
+
+    cy.contains('.rt-tr.-odd', name)
+      .should('contain', author)
+      .and('contain', publisher)
+      .find('#delete-record-undefined')
+      .click();
+
+    cy.get('#closeSmallModal-ok').click();
+
+    cy.contains(author, name, publisher).should('not.exist');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,4 @@
-// ***********************************************
+// *******************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
@@ -6,7 +6,7 @@
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
-// ***********************************************
+// *******************************************
 //
 //
 // -- This is a parent command --
@@ -23,3 +23,40 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+  const username = 'test123';
+  const Password = 'Test123!';
+
+  cy.visit('https://demoqa.com/login');
+
+  cy.get('#userName').click();
+
+  cy.get('#userName').type(username);
+
+  cy.get('#password').click();
+
+  cy.get('#password').type(Password);
+
+  cy.get('#login').click();
+
+  cy.get('#userName-value').should('contain', username);
+
+  cy.url().should('include', '/profile');
+});
+
+// I also try to use the cy.request, but receive the error 'Because this error occurred during a before each', please, give me advice, what I did wrong
+
+// Cypress.Commands.add('login', (userName, password) => {
+
+//     cy.request('POST', 'https://demoqa.com/login',  {
+
+//             userName: userName,
+//             password: password
+
+//    })
+//         .then(response => {
+//           cy.setCookie('token', response.body.token)
+//       })
+
+// });
