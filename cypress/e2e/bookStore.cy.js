c788d94306bb0256f7ce740ff6ff663273594dc5
Vladyslav <vladyslav1819@gmail.com>
2023-07-18 16:53:30 +0300

    add advance solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -13,25 +13,37 @@ describe('Book Store app', () => {
     description: 'Like it or not, Javascript is everywhere these days'
   };
 
-  const alertMessage = {
-    deleted: 'Book deleted.'
-  };
-
   before(() => {
     cy.visit('/login');
   });
 
-  it('should allow to login, add a book, and delete the book', () => {
+  it('should allow to login', () => {
     cy.findByPlaceholder('UserName').type(user.username);
     cy.findByPlaceholder('Password').type(user.password);
     cy.get('#login').click();
     cy.url().should('include', '/login');
     cy.get('#userName-value').should('contain', user.username);
+  });
+
+  it('should allow to add a book', () => {
+    cy.login();
+    cy.visit('/profile');
     cy.get('#gotoStore').click();
     cy.findByPlaceholder('Type to search').type(book.title);
     cy.contains('a', book.title).click();
     cy.get('#title-wrapper').should('contain', book.title);
     cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000);
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Book added to your collection.');
+    });
+    cy.visit('profile');
+    cy.contains('a', book.title).should('be.visible');
+  });
+
+  it('should allow to delete a book', () => {
+    cy.login();
     cy.visit('/profile');
     cy.contains('a', book.title).should('be.visible');
     cy.contains('[role="row"]', book.title).find('[title = "Delete"]')
@@ -39,7 +51,7 @@ describe('Book Store app', () => {
 
     cy.get('#closeSmallModal-ok').click();
     cy.on('window:alert', (str) => {
-      expect(str).to.equal(alertMessage.deleted);
+      expect(str).to.equal('Book deleted.');
     });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,19 @@
 Cypress.Commands.add('findByPlaceholder', (placeholder) => {
   cy.get(`[placeholder="${placeholder}"]`);
 });
+
+Cypress.Commands.add('login', (username = 'Adam', password = '12345Qwert!') => {
+  cy.request({
+    method: 'POST',
+    url: 'https://demoqa.com/Account/v1/Login',
+    body: {
+      userName: username,
+      password
+    }
+  }).then((response) => {
+    cy.setCookie('token', response.body.token);
+    cy.setCookie('userID', response.body.userId);
+    cy.setCookie('expires', response.body.expires);
+    cy.setCookie('userName', response.body.username);
+  });
+});
