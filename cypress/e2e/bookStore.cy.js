36f708781df91d971a9f014655943499fc0e78a9
Olena Lapa <olena.lapa@gmail.com>
2023-07-19 14:10:57 +0300

    last changes

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -11,6 +11,10 @@ describe('Book Store app', () => {
     author: 'Axel Rauschmayer',
     description: 'Like it or not, JavaScript is everywhere'
   };
+  const alertMessage = {
+    added: 'Book added to your collection.',
+    deleted: 'Book deleted'
+  };
 
   before(() => {
     cy.visit('/');
@@ -24,30 +28,38 @@ describe('Book Store app', () => {
     cy.get('#userName-value').should('be.visible');
     cy.url().should('include', '/profile');
     cy.visit('/login');
+  });
 
-    it('should allow to add a book in user collection', () => {
-      cy.login(user.userName, user.password);
-      cy.get(':nth-child(6) > .element-list > .menu-list > #item-2 > .text')
-        .click();
-      cy.get('#searchBox').type(book.name);
-      cy.contains('a', book.name).click();
-      cy.get('#description-wrapper').should('contain', book.description);
-      cy.on('window:alert', (str) => {
-        expect(str).to.equal(`Book added to your collection.`);
-        cy.visit('/profile');
-        cy.contains('a', 'Speaking JavaScript').should('be.visible');
-      });
+  it('should allow to add a book in user collection', () => {
+    cy.login(user.username, user.password);
+    cy.visit('/profile');
+    cy.get('#gotoStore')
+      .click();
+    cy.get('#searchBox').type(book.name);
+    cy.contains('a', book.name).click();
+    cy.get('#description-wrapper').should('contain', book.description);
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`);
+      cy.visit('/profile');
+      cy.contains('a', 'Speaking JavaScript').should('be.visible');
+    });
+  });
 
-      it('should allow to delete the book from user collection', () => {
-        cy.login(user.userName, user.password);
-        cy.visit('/profile');
-        cy.contains('a', 'Speaking JavaScript').should('be.visible');
-        cy.get('#delete-record-undefined').click();
-        cy.get('#closeSmallModal-ok').click();
-        cy.on('window:alert', (str) => {
-          expect(str).to.equal(`Book deleted.`);
-        });
-      });
+  it.only('should allow to delete the book from user collection', () => {
+    cy.login(user.username, user.password);
+    cy.visit('/profile');
+    cy.get('#gotoStore').click();
+    cy.get('#searchBox').type(book.name);
+    cy.contains('a', book.name).click();
+    cy.get('.text-right > #addNewRecordButton').click();
+    cy.visit('/profile');
+    cy.get('#delete-record-undefined').click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal(alertMessage.deleted);
+    });
+    cy.get('#closeSmallModal-ok').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book deleted.`);
     });
   });
 });
