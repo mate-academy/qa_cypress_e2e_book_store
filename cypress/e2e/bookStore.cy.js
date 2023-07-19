7379d05f55e775834abd916b33cc3a4b6541af09
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-07-19 17:56:02 +0300

    made small fixes to the second and third tests

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -50,18 +50,40 @@ describe('Book Store app', () => {
     cy.visit('/profile');
     cy.contains('a', book.title).should('be.visible');
     cy.contains(book.author).should('be.visible');
+
+    cy.get('#delete-record-undefined').click();
+    cy.get('#closeSmallModal-ok').click();
   });
 
   it('should delete book from user collection', () => {
     cy.login();
     cy.visit('/profile');
 
+    cy.contains('Go To Book Store').click();
+    cy.findByPlaceholder('Type to search').type(book.title);
+    cy.contains('a', book.title).click();
+
+    cy.get('#title-wrapper').should('contain', book.title);
+    cy.get('#description-wrapper').should('contain', book.description);
+
+    cy.contains('Add To Your Collection').click();
+
+    cy.visit('/profile');
+    cy.contains('a', book.title).should('be.visible');
+    cy.contains(book.author).should('be.visible');
+
     cy.get('#delete-record-undefined').click();
     cy.get('#closeSmallModal-ok').click();
 
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(alerMessage.deleted);
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal(alerMessage.added);
+      cy.removeAllListeners('window:alert');
+      cy.on('window:alert', (secondAlert) => {
+        expect(secondAlert).to.equal(alerMessage.deleted);
+      });
     });
+
+    cy.visit('/profile');
 
     cy.contains('a', book.title).should('not.exist');
   });
