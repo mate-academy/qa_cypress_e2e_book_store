ccb0e0404a38ae8c140979e92b31de1fd1e22c12
Denys Mandebura <mandebura1802@gmail.com>
2023-07-21 09:34:35 +0300

    demoblazetest

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -4,12 +4,12 @@ describe('Book Store app', () => {
   const user = {
     username: "Adamio12",
     password: "Keklol1!"
-  }
+  };
 
   const book = {
     description: 'Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o',
     title: 'Speaking JavaScript'
-  }
+  };
 
   before(() => {
     cy.visit('/login');
@@ -46,6 +46,5 @@ describe('Book Store app', () => {
     cy.get('#closeSmallModal-ok').click();
 
     cy.visit('/profile');
-
   });
 });
