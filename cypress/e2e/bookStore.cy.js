6e040ec6c09eb2e392b68d8e1d6d95ff67d04f88
Maxim <maksymb.eu@gmail.com>
2023-09-19 14:53:58 +0300

    My fourth cypress test

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,36 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  const username = 'tester228';
+  const password = 'euyD~@)6cMcKtd6';
 
+  before(() => {
+    cy.login(username, password);
   });
 
-  it('', () => {
+  it('should add a book to the user"s account', () => {
+    cy.url().should('contain', 'profile');
 
+    cy.get('.menu-list').contains('Book Store').click();
+    cy.findByPlaceholder('Type to search').type('Speaking JavaScript{enter}');
+    cy.get('a[href="/books?book=9781449365035"]').click();
+
+    cy.get('#description-wrapper').should('contain', 'Like it or not');
+    cy.get('.text-right #addNewRecordButton').click();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Book added to your collection.')
+        .click({ enter: true });
+    });
+
+    cy.get('.menu-list').contains('Profile').click();
+    cy.get('a[href="/books?book=9781449365035"]');
+
+    cy.get('[title="Delete"]').click();
+    cy.get('[id="closeSmallModal-ok"]').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Do you want to delete this book?')
+        .click({ enter: true });
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types='cypress' />
+
+Cypress.on('uncaught:exception', () => {
+  return false;
+});
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  return cy.get(`[placeholder='${placeholder}']`);
+});
+
+Cypress.Commands.add('login', (username, password) => {
+  cy.visit('https://demoqa.com/login');
+  cy.findByPlaceholder('UserName').type(username);
+  cy.findByPlaceholder('Password').type(password);
+  cy.get('#login').click();
+});
