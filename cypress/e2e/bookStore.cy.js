46a410b77f68db606f415ecc757a4e71d999843a
Svitlana Zai <svetkaqq1991@gmail.com>
2023-09-19 21:50:51 +0300

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -7,11 +7,13 @@ describe('Book Store app', () => {
 
   const username = 'naidaBecker';
   const password = 'Test123#';
-  const alertTextAdded = 'Book added to your collection.';
+  const alertText = {
+    alertTextAdded: 'Book added to your collection.',
+    alertTextDeleted: 'Book deleted.'
+  };
+
   const book = {
     title: 'Speaking JavaScript',
-    author: 'Axel Rauschmayer',
-    publisher: 'O\'Reilly Media',
     description: 'Like it or not, JavaScript is everywhere these days'
   };
 
@@ -20,25 +22,40 @@ describe('Book Store app', () => {
     cy.findByPlaceholder('UserName').type(username);
     cy.findByPlaceholder('Password').type(password);
     cy.get('#login').click();
+
     cy.urlShouldContainText('profile');
     cy.get('#userName-value').should('contain', 'naidaBecker');
+  });
+
+  it('should provide an ability to add book to the collection', () => {
+    cy.login(username, password);
+    cy.urlShouldContainText('profile');
 
     cy.get('.menu-list').contains('Book Store').click();
     cy.urlShouldContainText('books');
+
     cy.findByPlaceholder('Type to search').type(book.title);
     cy.get('.action-buttons').contains(book.title).click();
     cy.get('.profile-wrapper').should('exist');
     cy.get('#description-wrapper').should('contain', book.description);
     cy.get('.text-right #addNewRecordButton').click();
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(alertTextAdded);
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.contains(alertText.alertTextAdded);
     });
+  });
 
-    cy.contains('#item-3', 'Profile').click();
+  it('should provide an ability to delete a book from the collection', () => {
+    cy.login(username, password);
+    cy.urlShouldContainText('profile');
+
+    cy.get('.menu-list').contains('Profile').click();
     cy.get('.rt-table').should('contain', book.title);
     cy.get('#delete-record-undefined').click();
     cy.get('.modal-content').should('exist');
     cy.get('.modal-body').should('contain', 'Do you want to delete this book?');
     cy.get('#closeSmallModal-ok').click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal(alertText.alertTextDeleted);
+    });
   });
 });
