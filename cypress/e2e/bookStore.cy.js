abcd5b102f4d617cd7c3c2f5d5f6cb0db6e23ba2
InnaOleksyn543 <inna.oleksyn@outlook.com>
2023-09-21 21:01:07 +0300

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -4,7 +4,7 @@ describe('Book Store app', () => {
   const username = 'iinna';
   const password = 'Inna@12345';
   const titleOfBook = 'Speaking JavaScript';
-
+  
   beforeEach(() => {
     cy.visit('https://demoqa.com/login');
   });
@@ -22,22 +22,23 @@ describe('Book Store app', () => {
     cy.get('#userName-value').should('contain', username);
 
     cy.get('#gotoStore').click({ force: true });
-    cy.get('#searchBox').type('Spea');
+    cy.get('[placeholder="Type to search"]').type('Spea');
     cy.get('.action-buttons').click();
-    cy.get('#description-wrapper > .col-md-9 > #userName-value').should('contain', 'Like it or not, JavaScript is everywhere these');
+    cy.get('#description-wrapper').should('contain', 'Like it or not, JavaScript is everywhere these');
     cy.get('.text-right > #addNewRecordButton').click({ force: true });
     cy.on('window:alert', (str) => {expect(str).to.equal('Book added to your collection.');
     });
-    cy.get(':nth-child(6) > .element-list > .menu-list > #item-3').click();
-    cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)').should('contain', titleOfBook);
+    cy.get('.menu-list').contains('Profile').click();
+    cy.get('.rt-table').should('contain', titleOfBook);
   });
 
   it('should delete book', () => {
     cy.login(username, password);
     cy.get('#userName-value').should('contain', username);
 
-    cy.get(':nth-child(6) > .element-list > .menu-list > #item-3').click();
-    cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)').should('contain', titleOfBook);
+    cy.get('.menu-list').contains('Profile').click();
+    cy.get('.rt-table').should('contain', titleOfBook);
+
 
     cy.get('#delete-record-undefined > svg').click();
     cy.get('#closeSmallModal-ok').click();
