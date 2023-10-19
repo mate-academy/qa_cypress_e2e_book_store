76f4f67a16ce7ad9d555a57a0a02c68c96458cef
Artem Kravets <kravetsartyom@gmail.com>
2023-10-19 13:06:00 +0300

    fixed some details

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -14,39 +14,27 @@ describe('Book Store app', () => {
     deleted: 'Book deleted.'
   };
 
-  before(() => {
+  beforeEach(() => {
     cy.visit('https://demoqa.com/login');
     cy.viewport(1920, 1080);
+
+    cy.getByPlaceholder('UserName').type(userName);
+    cy.getByPlaceholder('Password').type(myPassword);
+    cy.get('#login').click();
+    cy.url().should('contain', 'profile');
   });
 
-  it('should successfully loggin the user with valid credentials', () => {
-    cy.getByPlaceholder('UserName').type(userName);
-
-    cy.getByPlaceholder('Password').type(myPassword);
-
-    cy.get('#login').click();
-
-    cy.url().should('contain', 'profile');
-
+  it('should successfully log in the user with valid credentials', () => {
     cy.get('#userName-value').should('exist').and('contain', userName);
   });
+
   it('should provide ability to add book to the Cart', () => {
-    cy.login(userName, myPassword);
-
-    cy.url().should('contain', 'profile');
-
     cy.get('.menu-list').contains('Book Store').click();
-
     cy.getByPlaceholder('Type to search').type(book.title);
-
     cy.get('#basic-addon2').click();
-
     cy.get('[href="/books?book=9781449365035"]').click();
-
     cy.get('#description-label').should('exist');
-
     cy.get('#description-wrapper').should('contain', book.desription);
-
     cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
 
     cy.on('window:alert', (alert) => {
@@ -54,19 +42,12 @@ describe('Book Store app', () => {
     });
 
     cy.contains('#item-3', 'Profile').click();
-
     cy.get('.rt-table').should('contain', book.title).and('contain', 'Axel Ra');
   });
 
   it('should provide ability to delete book from the Cart', () => {
-    cy.login(userName, myPassword);
-
-    cy.url().should('contain', 'profile');
-
     cy.get('#delete-record-undefined').click();
-
     cy.get('.modal-body').should('contain', 'Do you want to delete this book?');
-
     cy.get('#closeSmallModal-ok').click();
 
     cy.on('window:alert', (alert) => {
