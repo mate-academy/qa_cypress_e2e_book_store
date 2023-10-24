975ca51bc85d3e09d9df8686e577113b31889061
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-10-24 22:19:49 +0200

    solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,58 @@
 /// <reference types='cypress' />
+//const { generateUser } = require("../support/generate");
 
-describe('Book Store app', () => {
-  before(() => {
+describe("", () => {
+	before(() => {
+		cy.visit("https://demoqa.com/login");
+	});
 
-  });
+	it("should log in the user succesfully", () => {
+		//const { firstName, lastName, email } = generateUser();
+		cy.visit("https://demoqa.com/login");
+		cy.get("#userName").type("tola");
+		cy.get("#password").type("1qaz2wsxA!");
+		cy.get("#login")
+			.click()
+			.then(() => {
+				cy.get("#userName-value").should("contain.text", "tola");
+				cy.url().should("eql", "https://demoqa.com/profile");
+			});
 
-  it('', () => {
+		cy.get(":nth-child(6) > .element-list > .menu-list > #item-2 > .text")
+			.click()
+			.then(() => {
+				cy.url().should("eql", "https://demoqa.com/books");
+			});
 
-  });
+		cy.get("#searchBox").type("Speaking JavaScript{enter}");
+		cy.get('a[href="/books?book=9781449365035"]')
+			.click()
+			.then(() => {
+				cy.url().should("eql", "https://demoqa.com/books?book=9781449365035");
+				cy.get("#description-label").should("be.visible");
+				cy.get("#description-wrapper > .col-md-9 > #userName-value").should(
+					"contain.text",
+					"Like it or not, JavaScript is everywhere these days-from browser to server to mobile-and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who o"
+				);
+			});
+		cy.get("#addNewRecordButton")
+			.click({ force: true })
+			.then(() => {
+				cy.on("window:alert", (str) => {
+					expect(str).to.equal(`Book added to your collection.`);
+				});
+			});
+
+		cy.get(":nth-child(6) > .element-list > .menu-list > #item-3")
+			.click({ force: true })
+			.then(() => {
+				cy.url().should("eql", "https://demoqa.com/profile");
+				cy.get(".rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)").should(
+					"be.visible"
+				);
+			});
+		cy.get(
+			'.rt-tbody > :nth-child(1) > .rt-tr > [style="flex: 70 0 auto; width: 70px; max-width: 70px;"]'
+		).click();
+	});
 });
