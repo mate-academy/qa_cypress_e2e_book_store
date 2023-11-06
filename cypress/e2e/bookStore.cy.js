a1aa65ed468813d3b10063c976f635655e86206a
lognisgregnis <logsnskiy@ukr.net>
2023-11-06 17:14:42 +0200

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,62 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  const username = 'test_qa';
+  const password = 'Qwerty123@';
 
+  const book = {
+    title: 'Speaking JavaScript',
+    description: 'Like it or not, JavaScript is everywhere these days'
+  };
+
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+    cy.visit('https://demoqa.com/login');
+    cy.get('#userName')
+      .type(username);
+    cy.get('#password')
+      .type(password);
+    cy.get('#login')
+      .click();
+    cy.get('#userName-value')
+      .should('contain', username);
+    cy.url()
+      .should('include', '/profile');
   });
 
-  it('', () => {
+  it('should provide ability to add a book', () => {
+    cy.get('#gotoStore')
+      .click();
 
+    cy.get('#searchBox')
+      .type(book.title);
+
+    cy.get('.rt-table')
+      .contains(book.title)
+      .click();
+
+    cy.get('#description-wrapper')
+      .should('contain', book.description);
+
+    cy.get('.text-right #addNewRecordButton')
+      .click();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`);
+    });
+  });
+
+  it('should provide ability to delete a book', () => {
+    cy.get('.menu-list').contains('Profile')
+      .click();
+    cy.get('.rt-table')
+      .should('contain', book.title);
+    cy.get('[title="Delete"]')
+      .click();
+    cy.get('#closeSmallModal-ok')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book deleted.`);
+    });
   });
 });
