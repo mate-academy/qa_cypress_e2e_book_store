55dbf3ce35d64f060b8252cf91492db14c690f31
Solomiia Martyniuk <solomiia.e@gmail.com>
2023-11-06 17:28:34 +0200

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,51 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  const user = {
+    username: 'v66',
+    password: 'Pa7$7777'
+  }
 
+  const book = {
+    title: 'Speaking JavaScript',
+    description: 'Like it or not, JavaScript is everywhere'
+  }
+
+  before(() => {
+    cy.visit('/login');
   });
 
-  it('', () => {
-
+  it('should provide the ability to login, search, add and delete book', () => {
+    cy.get('#userName')
+      .type(user.username);
+    cy.get('#password')
+      .type(user.password);
+    cy.get('#login')
+      .click();
+    cy.get('#userName-value')
+      .should('contain', user.username);
+    cy.url()
+      .should('contain', '/profile');
+    cy.visit('/profile');
+    cy.get('#gotoStore')
+      .click();
+    cy.get('#searchBox')
+      .type(book.title);
+    cy.get('.mr-2')
+      .click();
+    cy.get('.col-md-9')
+      .should('contain', book.description);
+    cy.get('.text-right > #addNewRecordButton')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`)
+    })
+    cy.visit('/profile')
+    cy.get('.mr-2')
+      .should('contain', book.title);
+    cy.get('delete-record-undefined')
+      .click();
+    cy.get('closeSmallModal-ok')
+      .click();
   });
 });
