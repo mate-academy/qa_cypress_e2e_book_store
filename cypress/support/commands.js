6a78d5903586f4c2797111e4cb712529b5dab4a8
Snizhana Broniar <sky.fantasy66@gmail.com>
2023-11-06 16:34:30 +0200

    SOLUTION

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (UserName, Password) => {
+  cy.get('#userName').type(UserName);
+  cy.get('#password').type(Password);
+  cy.get('#login').click();
+});
