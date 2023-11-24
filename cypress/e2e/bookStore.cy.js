62f61f74688bbf244bae3568afa10a6c3bc9acd1
Julia14012000 <Juliamilan1414@gmail.com>
2023-11-24 12:09:41 +0200

    Fixed comments

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,59 +1,80 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  const username = 'test123';
-  const password = 'Test123!';
+  const user = {
+    username: 'test123',
+    password: 'Test123!'
+  };
+
+  const book = {
+    title: 'Speaking JavaScript',
+    author: 'Axel Rauschmayer',
+    publisher: 'O\'Reilly Media',
+    pages: '460',
+    description: 'Like it or not, JavaScript is everywhere these days-from'
+  };
+
+  const allert = {
+    added: 'Book added to your collection.',
+    deleted: 'Book deleted.'
+  };
 
   beforeEach(() => {
-    cy.login(username, password);
-
-    cy.visit('https://demoqa.com/login');
+    cy.visit('/login');
   });
 
-  it('should display username at the page after login', () => {
+  it('Should be possible to log in with valid data', () => {
+    cy.get('#userName')
+      .type(user.username);
+    cy.get('#password')
+      .type(user.password);
+    cy.get('#login')
+      .click();
     cy.get('#userName-value')
-      .should('contain', username);
+      .should('contain', user.username);
+    cy.url()
+      .should('contain', '/profile');
   });
 
-  const name = 'Speaking JavaScript';
-  const author = 'Axel Rauschmayer';
-  const publisher = 'O\'Reilly Media';
-
-  it('should provide an ability to find and add book', () => {
-    cy.contains('#item-2', 'Book Store').click();
-
-    cy.url().should('include', '/books');
-
-    cy.get('#searchBox').type(`${name}{enter}`);
-
-    cy.get('.ReactTable').should('contain', name, author, publisher);
-
-    cy.contains('.rt-tr.-odd', name)
-      .should('contain', author)
-      .and('contain', publisher);
-
-    cy.contains('.action-buttons', name).click();
-
-    cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
-
+  it('Should be possible to search book and add to the collection', () => {
+    cy.login(user.username, user.password);
+    cy.visit('/profile');
+    cy.get('#userName-value')
+      .should('contain', user.username);
+    cy.get('#gotoStore')
+      .click();
+    cy.get('#searchBox')
+      .type(book.title);
+    cy.get('[href="/books?book=9781449365035"]')
+      .click();
+    cy.get('.profile-wrapper')
+      .should('contain', book.title)
+      .and('contain', book.author)
+      .and('contain', book.publisher)
+      .and('contain', book.pages)
+      .and('contain', book.description);
+    cy.contains('#addNewRecordButton', 'Add To Your Collection')
+      .click();
     cy.on('window:alert', (str) => {
-      expect(str).to.equal('Book added to your collection.');
+      expect(str).to.equal(allert.added);
     });
   });
 
-  it('should provide an ability to delete book', () => {
-    cy.contains('#item-3', 'Profile').click();
-
-    cy.url().should('include', '/profile');
-
-    cy.contains('.rt-tr.-odd', name)
-      .should('contain', author)
-      .and('contain', publisher)
-      .find('#delete-record-undefined')
+  it('Should be possible to delete a book from the collection', () => {
+    cy.login(user.username, user.password);
+    cy.visit('/profile');
+    cy.get('#userName-value')
+      .should('contain', user.username);
+    cy.get('[role="row"]')
+      .should('contain', book.title)
+      .and('contain', book.author)
+      .and('contain', book.publisher);
+    cy.get('#delete-record-undefined')
       .click();
-
-    cy.get('#closeSmallModal-ok').click();
-
-    cy.contains(author, name, publisher).should('not.exist');
+    cy.get('#closeSmallModal-ok')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(allert.deleted);
+    });
   });
 });
