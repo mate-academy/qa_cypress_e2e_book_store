a2bc50876c7205692a042e920c0889203fce510a
Julia14012000 <Juliamilan1414@gmail.com>
2023-11-28 10:28:06 +0200

    Fixed Login request

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,16 +26,16 @@
 Cypress.Commands.add('login', (username, password) => {
   cy.request({
     method: 'POST',
-    url: '/Account/v1/Login',
+    url: 'https://demoqa.com/Account/v1/Login',
+    form: true,
     body: {
       userName: username,
       password
     }
   }).then((response) => {
-    cy.setCookie('userID', response.body.userId);
-    cy.setCookie('userName', response.body.username);
     cy.setCookie('token', response.body.token);
+    cy.setCookie('userName', response.body.username);
+    cy.setCookie('userID', response.body.userId);
     cy.setCookie('expires', response.body.expires);
   });
-}
-);
+});
