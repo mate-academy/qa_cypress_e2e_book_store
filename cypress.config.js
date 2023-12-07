5097bc37b52ccdc5e28f3f3adaf6c4590a97d206
Wojciech Sęk <wojtasskate@wp.pl>
2023-12-07 17:54:24 +0100

    version1

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1320,
   e2e: {
     setupNodeEvents(on, config) {
     }
-  }
+  },
+  responseTimeout: 6000,
+  watchForFileChanges: false
 });
--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,53 @@
 /// <reference types='cypress' />
-
 describe('Book Store app', () => {
-  before(() => {
+  const testUser = {
+    username: 'wojtasck',
+    password: 'Wojtas1234!',
+    book: 'Speaking JavaScript',
+    description: 'Like it or not, JavaScript is everywhere these'
+  };
 
+  it('should login user with correct test data', () => {
+    cy.visit('https://demoqa.com/login');
+    cy.get('#userName')
+      .type(testUser.username);
+    cy.get('#password')
+      .type(testUser.password);
+    cy.get('#login')
+      .click();
+    cy.url()
+      .should('contain', 'https://demoqa.com/profile');
+    cy.get('#userName-value')
+      .should('contain', testUser.username);
   });
 
-  it('', () => {
+  it('should let logged in user add a book to their collection', () => {
+    cy.login(testUser.username, testUser.password);
+    cy.visit('https://demoqa.com/profile');
+    cy.contains('#item-2', 'Book Store')
+      .click();
+    cy.get('#searchBox')
+      .type(testUser.book);
+    cy.contains('a', `${testUser.book}`)
+      .click();
+    cy.contains('#userName-value', `${testUser.description}`)
+      .should('exist');
+    cy.get('#addNewRecordButton')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`);
+    });
+  });
 
+  it('should let logged in user remove a book in their possesion', () => {
+    cy.login(testUser.username, testUser.password);
+    cy.visit('https://demoqa.com/profile');
+    cy.contains('#item-3', 'Profile')
+      .click();
+    cy.contains('[role="row"]', testUser.book)
+      .get('#delete-record-undefined')
+      .click();
+    cy.get('#closeSmallModal-ok')
+      .click();
   });
 });
