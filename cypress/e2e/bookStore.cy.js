e217a0bcfcc40509d4953ca57f5692face4c8796
Mateusz Gajdzinski <tulu17@gmail.com>
2023-12-07 12:10:58 +0100

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,55 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
-
+  beforeEach(() => {
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should log in existing user', () => {
+    cy.get('#userName')
+      .type('testingagain');
+    cy.get('#password')
+      .type('Testingagain123$');
+    cy.get('#login')
+      .click();
+    cy.url()
+      .should('include', '/profile');
+    cy.get('#userName-value')
+      .should('contain', 'testingagain');
+  });
 
+  it('should add a book', () => {
+    cy.login();
+    cy.visit('/profile');
+    cy.get('#gotoStore')
+      .click();
+    cy.get('#searchBox')
+      .type('Speaking JavaScript');
+    cy.contains('[role="row"]', 'Speaking JavaScript')
+      .should('contain', 'Axel Rauschmayer')
+      .and('contain', 'O\'Reilly Media');
+    cy.contains('a', 'Speaking JavaScript')
+      .click();
+    cy.get('#description-label')
+      .should('contain', '');
+    cy.contains('#addNewRecordButton', 'Add To Your Collection')
+      .click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('Book added to your collection');
+    });
+  });
+
+  it('should delete book', () => {
+    cy.login();
+    cy.visit('/profile');
+    cy.get('.menu-list')
+      .contains('Profile')
+      .click();
+    cy.get('.rt-table')
+      .should('contain', 'Speaking JavaScript');
+    cy.get('#delete-record-undefined')
+      .click();
+    cy.get('#closeSmallModal-ok')
+      .click();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// eslint-disable-next-line max-len
+Cypress.Commands.add('login', (username = 'testingagain', pass = 'Testingagain123$') => {
+  cy.request({
+    method: 'POST',
+    url: 'https://demoqa.com/Account/v1/Login',
+    body: {
+      userName: username,
+      password: pass
+    }
+  }).then((response) => {
+    cy.setCookie('token', response.body.token);
+    cy.setCookie('userID', response.body.userId);
+    cy.setCookie('expires', response.body.expires);
+    cy.setCookie('username', response.body.username);
+  });
+});
