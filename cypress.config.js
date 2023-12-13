49a457dcc25fcdb81583fe9367dc386b19109b01
Dawid_Antkiewicz <dawidantkiew@gmail.com>
2023-12-13 14:12:28 +0100

    Fixed solution2

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    userAgent: null,
     viewportHeight: 1200,
     viewportWidth: 1000,
     baseUrl: 'https://demoqa.com',
--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -33,10 +33,19 @@ it("should add book to the user's profile", () => {
 })
 });
 
-it('should go to the profile page and delete the book', () => {
+it.only('should go to the profile page and delete the book', () => {
   //Loging by a request
   cy.visit('/login');
   cy.login(user);
+  //Go to the Books content
+  cy.get('.menu-list').contains('Book Store').click();
+  cy.url().should('contain', '/books');
+  //Find the book
+  cy.get('[placeholder="Type to search"]').type(book.title);
+  cy.get('a').contains(book.title).click();
+  cy.get('.form-label').should('contain.text', book.description)
+  //Add the book to the cellection
+  cy.get('.text-right.fullButton').contains('Add To Your Collection').click();
   //Delete book from the collection
   cy.visit('/profile');
   cy.get('a').contains(book.title).should('exist');  
