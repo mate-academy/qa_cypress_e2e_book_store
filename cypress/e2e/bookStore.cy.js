389ff2d0492cf02723f7612dfcd1334631caea2f
Paweł Grzywna <pawelgrzywna01@outlook.com>
2023-12-14 20:00:22 +0100

    Solution2

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -2,28 +2,33 @@
 
 describe('Book Store app', () => {
   before(() => {
-    cy.visit('https://demoqa.com/books');
+    cy.visit('https://demoqa.com/login');
     cy.viewport(1000, 1200);
   });
 
   // eslint-disable-next-line max-len
   it('should be able to login, add book to the collection and delete it', () => {
-    cy.get('#login').click();
+    let firstAlertHandled = false;
     cy.get('#userName').type('pawgrzy');
     cy.get('#password').type('Qwer123!');
     cy.get('#login').click();
     cy.get('#userName-value').should('contain', 'pawgrzy');
-    cy.url().should('include', '/books');
+    cy.url().should('include', '/profile');
+    cy.get('.menu-list').contains('Book Store').click();
     cy.get('#searchBox').type('Speaking JavaScript');
     cy.get('[href="/books?book=9781449365035"]').click();
     // eslint-disable-next-line max-len
     cy.get('.form-label').should('contain.text', 'Like it or not, JavaScript is everywhere');
-    cy.get('.text-right > #addNewRecordButton').click();
+    cy.get('[type="button"]').contains('Add To Your Collection').click();
     cy.on('window:alert', (str) => {
-      expect(str).to.equal(`Book added to your collection.`);
+      if (!firstAlertHandled) {
+        expect(str).to.equal('Book added to your collection.');
+        firstAlertHandled = true;
+      } else {
+        expect(str).to.equal('Book deleted.');
+      };
     });
-    // eslint-disable-next-line cypress/no-force, max-len
-    cy.get(':nth-child(6) > .element-list > .menu-list > #item-3').click({ force: true });
+    cy.get('.menu-list').contains('Profile').click();
     cy.get('.profile-wrapper').should('contain', 'Speaking JavaScript');
     cy.get('#delete-record-undefined').click();
     cy.get('#closeSmallModal-ok').click();
