3b3c30dbd0fab1a71bf6b56b2c8fe6a39a16a702
KatarzynaKolodziejczyk <kolodziejczyk.katarzyna09@gmail.com>
2023-12-22 15:17:22 +0100

    Test Book Store

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,28 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('findByPlaceholder', (placeholder, tag = 'input') => {
+  cy.get(`${tag}[placeholder="${placeholder}"]`);
+});
+
+Cypress.Commands.add('login', (user) => {
+  cy.request('POST', 'https://demoqa.com/Account/v1/Login', {
+    userName: user.userName,
+    password: user.password
+  }).then((response) => {
+    cy.setCookie('userID', response.body.userId);
+    cy.setCookie('token', response.body.token);
+    cy.setCookie('userName', response.body.username);
+    cy.setCookie('expires', response.body.expires);
+  });
+});
+
+Cypress.Commands.add('deleteBook', (isbnToDelete, userID) => {
+  cy.request('DELETE', 'https://demoqa.com/BookStore/v1/Book', {
+    isbn: isbnToDelete,
+    userId: userID
+  }).then((response) => {
+    expect(response.status).to.equal('204');
+  });
+});
