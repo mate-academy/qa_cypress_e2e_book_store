b788c506169f9ba483c6d8f2af1a2b89529bf605
Vasylyna Zvizlo <vasylynazvizlo@gmail.com>
2023-12-29 19:20:00 +0200

    test book store

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,49 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  const user = {
+    username: 'vasylynazvizlo',
+    password: '12345#Qw1'
+  };
 
+  const book = {
+    title: 'Speaking JavaScript',
+    author: 'Axel Rauschmayer',
+    description: 'Like it or not, JavaScript is everywhere these day'
+  };
+
+  it('should provide an ability to log in', () => {
+    cy.visit('/login');
+    cy.findByPlaceholder('UserName').type(user.username);
+    cy.findByPlaceholder('Password').type(user.password);
+    cy.get('#login').click();
+    cy.url().should('include', '/profile');
+    cy.get('#userName-value').should('contain', user.username);
   });
 
-  it('', () => {
-
+  it('should allow to search for the book and add to collection', () => {
+    cy.login();
+    cy.visit('/books');
+    cy.get('#searchBox').type(book.title);
+    cy.contains('[role="row"]', book.title)
+      .should('contain', book.author);
+    cy.get('a').contains(book.title).click();
+    cy.get('#description-wrapper').should('contain', book.description);
+    cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('Book added to your collection.');
+    });
+    cy.contains('span', 'Profile').click();
+    cy.get('.mr-2').should('contain', 'Speaking JavaScript');
+  });
+  it('should allow to delete book from list', () => {
+    cy.login();
+    cy.visit('/profile');
+    cy.get('#delete-record-undefined').click();
+    cy.get('#closeSmallModal-ok').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book deleted.`);
+    });
+    cy.get('.rt-tbody').should('not.contain', 'Speaking JavaScript');
   });
 });
