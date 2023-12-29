b2673ee2b95c205a139ae3508bf4cdf3d867645d
Yura Kisilchuk <yurkis02@gmail.com>
2023-12-29 18:23:42 +0200

    added e2e tests for the login, searching, adding and deleting the book

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,23 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+    cy.get(`[placeholder="${placeholder}"]`);
+  });
+
+Cypress.Commands.add('login', (username, password) => {
+  cy.request({
+    method: 'POST',
+    url: 'https://demoqa.com/Account/v1/Login', 
+    body: {
+      userName: 'Yurii99',
+      password: 'Qwer1234!',
+    },
+  }).then((response) => {
+    cy.setCookie('token', response.body.token);
+    cy.setCookie('expires', response.body.expires);
+    cy.setCookie('userID', response.body.userId);
+    cy.setCookie('userName', response.body.username);
+
+  });
+});
