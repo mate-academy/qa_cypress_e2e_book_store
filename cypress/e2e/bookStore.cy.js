38608f59b01ce396863d1d9f73969a29c59aab13
Maciej Gajda <ma.gajda@gmail.com>
2023-12-29 19:36:08 +0100

    solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,32 +1,31 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  const username = 'stangetz';
+  const password = 'St@n6372';
+
+  beforeEach(() => {
     cy.visit('/login');
-  });
-
-  it('Bookstore task', () => {
-    const username = 'stangetz';
-    const password = 'St@n6372';
-
     cy.findByPlaceholder('UserName').type(username);
     cy.findByPlaceholder('Password').type(password);
     cy.get('button').contains('Login').click();
-    cy.get('.form-label').contains('stangetz');
-    cy.url().should('contains', '/profile');
+    cy.url().should('include', '/profile');
+  });
+  it('should add a book to the collection', () => {
     cy.get('#item-2 .text').contains('Book Store').click();
     cy.get('[placeholder="Type to search"]').type('Speaking JavaScript');
     cy.get('.action-buttons').click();
     cy.get('#ISBN-wrapper > .col-md-9 > #userName-value').should('exist');
     cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(`Book added to your collection.`);
-    });
     cy.get('#item-3 .text').contains('Profile').click();
     cy.get('.main-header').contains('Profile');
-    cy.get('div.profile-wrapper').should('contain', 'Speaking JavaScript');
+    cy.get('div.profile-wrapper').contains('Speaking JavaScript');
+  });
+  it('should delete a book from the collection', () => {
+    cy.get('#item-3 .text').contains('Profile').click();
+    cy.get('.main-header').contains('Profile');
+    cy.get('div.profile-wrapper').contains('Speaking JavaScript');
     cy.get('#delete-record-undefined').click();
     cy.get('#closeSmallModal-ok').click();
-    cy.reload();
   });
 });
