0d2e47a46fb4d339bb5a1f55b4bfe824702caf61
Yuliia Siromakha <yuliya.o.siromakha@gmail.com>
2023-12-30 22:05:28 +0200

    config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+
+    viewportHeight: 1080,
+    viewportWidth:1320,
+    baseUrl:'https://demoqa.com',
+
     setupNodeEvents(on, config) {
     }
   }
