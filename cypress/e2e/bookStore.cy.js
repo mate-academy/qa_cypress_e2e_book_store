0526723a6b5a8e732cceff06e803f73b78cbd694
Dmytro_Yelisieiev <deliseev0482@gmail.com>
2023-12-30 19:43:39 +0200

    Solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,54 @@
 /// <reference types='cypress' />
+const UserName = 'TestUser048';
+const Password = 'Qwerty01!';
+const book = {
+  title: 'Speaking JavaScript',
+  author: 'Axel Rauschmayer',
+  publisher: 'O\'Reilly Media',
+  description: 'Like it or not, JavaScript is everywhere these days'
+};
 
 describe('Book Store app', () => {
-  before(() => {
-
+  it('should have an ability to log in', () => {
+    cy.visit('/login');
+    cy.get('#userName')
+      .type(UserName);
+    cy.get('#password')
+      .type(Password);
+    cy.get('#login')
+      .click();
+    cy.contains('#userName-value', UserName);
+    cy.url().should('eq', 'https://demoqa.com/profile');
   });
 
-  it('', () => {
-
+  it.only('should navigate to BookStore', () => {
+    cy.login();
+    cy.visit('/books');
+    cy.get('#searchBox')
+      .type(book.title);
+    cy.contains('[role="row"]', book.title)
+      .should('contain', book.author)
+      .and('contain', book.publisher);
+    cy.get('.action-buttons')
+      .click();
+    cy.get('#description-wrapper')
+      .should('contain', book.description);
+    cy.contains('#addNewRecordButton', 'Add To Your Collection')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`);
+    });
+    cy.visit('/profile');
+    cy.contains('[role="row"]', book.title)
+      .should('contain', book.author)
+      .and('contain', book.publisher);
+    cy.get('#delete-record-undefined')
+      .click();
+    cy.contains('.modal-body', 'Do you want to delete this book?');
+    cy.contains('#closeSmallModal-ok', 'OK')
+      .click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book deleted.`);
+    });
   });
 });
