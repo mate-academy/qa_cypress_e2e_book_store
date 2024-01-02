962a37ae1fa16809f422035ee9015c9769fbaf26
Lyubov Nekroyenko <nekroyenko@gmail.com>
2024-01-02 17:19:52 +0200

    add tests login-add book to collection - delete book

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -26,7 +26,7 @@ describe('Book Store app', () => {
     cy.contains('#userName-value', user.username);
   });
 
-  it('should allow to add that is found to the user collection', () => {
+  it('should allow to add a book that is found to the user collection', () => {
     cy.login();
     cy.visit('/books');
     cy.findByPlaceholder('Type to search').type('Speaking JavaScript');
@@ -38,26 +38,17 @@ describe('Book Store app', () => {
     });
   });
 
-  it.only('should delete a book from the user collection', () => {
+  it('should delete a book from the user collection', () => {
     cy.login();
     cy.visit('/books');
-    cy.findByPlaceholder('Type to search').type('Speaking JavaScript');
-    cy.contains('.action-buttons', book.title).click();
-    cy.get('#description-wrapper').should('contain', book.description);
-    cy.contains('#addNewRecordButton', 'Add To Your Collection').click();
-    cy.on('window:alert', (alert) => {
-      expect(alert).to.equal('Book added to your collection.');
-    });
-    // cy.contains('.text-left > #addNewRecordButton').click();
-    cy.get().click(); // '/profile'
-    // cy.contains('#item-3', '.btn btn-light active').click();
-    cy.get('[style="flex: 70 0 auto; width: 70px; max-width: 70px;"]').click();
+    cy.get('.action-buttons').should('contain', book.title);
+    cy.contains('#item-3', 'Profile').click();
+    cy.get('#delete-record-undefined').click();
     cy.get('.modal-body').should('contain', 'Do you want to delete this book?');
     cy.get('#closeSmallModal-ok').click();
-    // cy.on('window:alert', (alert) => {
-    //  expect(alert).to.equal('Book deleted.');
-    // });
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('Book deleted.');
+    });
     cy.get('.ReactTable').should('not.contain', 'Speaking JavaScript');
   });
 });
-// cy.get('#gotoStore').should('contain', 'Go To Book Store').click();
