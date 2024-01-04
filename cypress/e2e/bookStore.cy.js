723a7512b4c07a74d664bd49d249384783dcfbd1
v.savchenko <v.savchenko@polytech.software>
2024-01-04 12:57:58 +0200

    Add first test

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,17 @@
 /// <reference types='cypress' />
 
-describe('Book Store app', () => {
-  before(() => {
+describe("Book Store app", () => {
+  //create a simple test
 
+  before(() => {
+    cy.visit("https://demoqa.com/books");
   });
 
-  it('', () => {
-
+  it("basic test", () => {
+    cy.get("#searchBox").type("Git Pocket Guide{enter}");
+    cy.get("#see-book-Git Pocket Guide").click();
+    cy.get("#addNewRecordButton").click();
+    cy.get("#userName").type("John Doe");
+    cy.get("#userEmail").type("some email");
   });
 });
