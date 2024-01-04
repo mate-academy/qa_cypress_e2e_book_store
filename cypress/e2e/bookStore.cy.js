cd65bfe412e4e01905fb3ee37ddcb3818d84a78b
Natalia-Slavuta <tritoutike@gmail.com>
2024-01-04 20:53:21 +0200

    bookstore tests

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,35 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  let user;
 
+  before(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
-
+  it('should allow to successfully log in', () => {
+    cy.visit('/login');
+    cy.findByPlaceholder('UserName').type(user.userName);
+    cy.findByPlaceholder('Password').type(user.password);
+    cy.get('#login').click();
+    cy.url().should('contain', '/profile');
+    cy.get('#userName-value').should('contain', user.userName);
+    cy.get('#gotoStore').click();
+    cy.findByPlaceholder('Type to search').type('Speaking JavaScript');
+    cy.get('.action-buttons').click();
+    cy.get('#title-wrapper').should('contain', 'Speaking JavaScript');
+    cy.get('#author-wrapper').should('contain', 'Axel Rauschmayer');
+    cy.get('#description-wrapper')
+      .should('contain', 'Like it or not, JavaScript is everywhere');
+    cy.get('.text-right.fullButton').click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Book added to your collection.`);
+    });
+    cy.visit('/profile');
+    cy.get('.action-buttons').should('contain', 'Speaking JavaScript');
+    cy.get('#delete-record-undefined').click();
+    cy.get('#closeSmallModal-ok').click();
   });
 });
