6591368b62a9c315e01ca9e6830cf5585cd67773
Vladyslav Ovsiannikov <vladovsiannikov07@gmail.com>
2024-03-15 19:52:22 +0200

    My solution

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,30 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
+  let user;
   before(() => {
-
+    cy.visit('/login');
+    user = {
+      username: 'TestingOvsianka',
+      password: 'Qwerty@12345'
+    };
   });
 
-  it('', () => {
+  it('should allow to login', () => {
+    cy.visit('/login');
+    cy.get('#userName').type(user.username);
+    cy.get('#password').type(user.password);
+    cy.get('#login').click();
+    cy.get('#userName-value').should('not.have.value', user.username);
+    cy.url().should('include', '/profile');
+  });
 
+  it('should allow to search for the book', () => {
+    cy.login();
+    cy.visit('/books');
+    cy.get('.form-control').type('Speaking JavaScript');
+    cy.get('.rt-table').should('contain.text', 'Speaking JavaScript');
+    cy.get('.rt-table').should('contain.text', 'Axel Rauschmayer');
+    cy.get('.rt-table').should('contain.text', '\'Reilly Media');
   });
 });
