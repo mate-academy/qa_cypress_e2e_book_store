e5ee484c4487ce35dbb580ae23fcd1251536cda9
Anatolii Zinevych <anatolii.zinevych@gmail.com>
2024-03-17 20:30:29 +0200

    added two tests

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,33 @@
 /// <reference types='cypress' />
 
 describe('Book Store app', () => {
-  before(() => {
+  const user = {
+    username: 'Flood',
+    password: 'qwert!Q12345'
+  };
 
+  it('should provide an ability to login', () => {
+    cy.visit('/login');
+    cy.get('#userName')
+      .type(user.username);
+    cy.get('#password')
+      .type(user.password);
+    cy.get('#login').click();
+    cy.get('#userName-value')
+      .should('contain', user.username);
+    cy.url().should('include', '/profile');
   });
 
-  it('', () => {
-
+  it('should allow to search for the book', () => {
+    cy.login();
+    cy.visit('/books');
+    cy.get('#searchBox')
+      .type('Speaking JavaScript');
+    cy.contains('a[href="/books?book=9781449365035"]', 'Speaking JavaScript')
+      .should('exist');
+    cy.contains('.rt-td', 'Axel Rauschmayer')
+      .should('exist');
+    cy.contains('.rt-td', "O'Reilly Media")
+      .should('exist');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import './commands';
+
+Cypress.Commands.add('login', () => {
+    cy.request({
+        method: 'POST',
+        url: 'https://demoqa.com/Account/v1/Login',
+        body: {
+            userName: 'Flood',
+            password: 'qwert!Q12345'
+        },
+    }).then((response) => {
+        cy.setCookie('token', response.body.token);
+        cy.setCookie('userID', response.body.userId);
+        cy.setCookie('userName', response.body.username);
+        cy.setCookie('expires', response.body.expires);
+    });
+});
