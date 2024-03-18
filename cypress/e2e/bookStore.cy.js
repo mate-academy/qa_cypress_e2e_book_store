935b9f5183c611df63ed0f8ad2fa3f5b8293e5e5
Пользователь <wetball74@gmail.com>
2024-03-18 02:28:51 +0200

    First try

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -1,11 +1,37 @@
-/// <reference types='cypress' />
-
+// <reference types='cypress' />
 describe('Book Store app', () => {
-  before(() => {
+  const user = {
+    name: 'Zakhar',
+    password: 'Zakhar123!'
+  };
 
+  before(() => {
+    cy.visit('/login');
   });
 
-  it('', () => {
+  it('should log in user', () => {
+    cy.get('#userName').type(user.name);
+    cy.get('#password').type(user.password);
+    cy.get('button#login').click();
+    cy.get('#userName-value').should('contain', user.name);
+  });
 
+  it('should find a book', () => {
+    const bookName = 'Speaking JavaScript';
+    const authorName = 'Axel Rauschmayer';
+    const publisher = 'O\'Reilly Media';
+
+    cy.login(user);
+
+    cy.visit('/books');
+
+    cy.get('#searchBox').type(bookName);
+
+    cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(2)')
+      .should('contain.text', bookName);
+    cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(3)')
+      .should('contain.text', authorName);
+    cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(4)')
+      .should('contain.text', publisher);
   });
 });
