b900bcb2005a9d2cb11f288525c5899fb60eae6d
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-04-25 15:25:52 +0200

    e2e book store tests

--- cypress/e2e/bookStore.cy.js
+++ cypress/e2e/bookStore.cy.js
@@ -2,10 +2,19 @@
 
 describe('Book Store app', () => {
   before(() => {
-
+    cy.visit('https://demoqa.com/');
   });
 
   it('', () => {
-
+    cy.get('.category-cards > :nth-child(6) > :nth-child(1)').click();
+    cy.get(':nth-child(6) > .element-list > .menu-list > #item-0').click();
+    cy.get('#userName').type('Hairy');
+    cy.get('#password').type('Qwerty123#');
+    cy.get('#login').click();
+    cy.get('#userName-value').should('contain', 'Hairy');
+    cy.url().should('eq', 'https://demoqa.com/profile');
+    cy.get(':nth-child(6) > .element-list > .menu-list > #item-2').click();
+    cy.get('#searchBox').type('Speaking JavaScript');
+    cy.get('[href="/books?book=9781449365035"]').click();
   });
 });
